0b9b760d249e7552f399c887cfee62da3ad721f0
Chaithra <chaithradm8@gmail.com>
2022-12-30 14:29:26 +0530

    implimented

--- src/main/java/com/example/library/studentlibrary/models/CardStatus.java
+++ src/main/java/com/example/library/studentlibrary/models/CardStatus.java
@@ -1,5 +1,11 @@
 package com.example.library.studentlibrary.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
 public enum CardStatus {
     ACTIVATED,
     DEACTIVATED
